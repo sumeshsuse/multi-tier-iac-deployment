60deefd9bba63e081352dffb125aa1dafcef6b94
sumeshsuse <sumeshsuseelan.techie@gmail.com>
2026-03-23 21:53:26 +0100

    Add Hetzner load balancer resources

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -11,13 +11,11 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
-resource "hcloud_ssh_key" "default" {
-  name       = "weather-key"
-  public_key = var.ssh_public_key
+data "hcloud_ssh_key" "default" {
+  name = "weather-key"
 }
 
 # Firewall
-
 resource "hcloud_firewall" "dev_firewall" {
   name = "dev-firewall"
 
@@ -50,8 +48,7 @@ resource "hcloud_firewall" "dev_firewall" {
   }
 }
 
-# API
-
+# API Servers
 module "api_servers" {
   source   = "../modules/servers"
   for_each = toset(var.api_servers)
@@ -61,12 +58,11 @@ module "api_servers" {
   server_image    = var.server_image
   server_location = var.server_location
 
-  ssh_keys     = [hcloud_ssh_key.default.id]
+  ssh_keys     = [data.hcloud_ssh_key.default.id]
   firewall_ids = [hcloud_firewall.dev_firewall.id]
 }
 
-# DB
-
+# DB Server
 module "db_server" {
   source = "../modules/servers"
 
@@ -75,6 +71,44 @@ module "db_server" {
   server_image    = var.server_image
   server_location = var.server_location
 
-  ssh_keys     = [hcloud_ssh_key.default.id]
+  ssh_keys     = [data.hcloud_ssh_key.default.id]
   firewall_ids = [hcloud_firewall.dev_firewall.id]
+}
+
+# Load Balancer
+resource "hcloud_load_balancer" "dev_lb" {
+  name               = "dev-lb"
+  load_balancer_type = "lb11"
+  location           = var.server_location
+}
+
+resource "hcloud_load_balancer_service" "dev_http" {
+  load_balancer_id = hcloud_load_balancer.dev_lb.id
+  protocol         = "http"
+  listen_port      = 80
+  destination_port = 8080
+
+  health_check {
+    protocol = "http"
+    port     = 8080
+    interval = 15
+    timeout  = 10
+    retries  = 3
+
+    http {
+      path = "/health"
+    }
+  }
+}
+
+resource "hcloud_load_balancer_target" "api_1" {
+  type             = "server"
+  load_balancer_id = hcloud_load_balancer.dev_lb.id
+  server_id        = module.api_servers["dev-api-1"].server_id
+}
+
+resource "hcloud_load_balancer_target" "api_2" {
+  type             = "server"
+  load_balancer_id = hcloud_load_balancer.dev_lb.id
+  server_id        = module.api_servers["dev-api-2"].server_id
 }
