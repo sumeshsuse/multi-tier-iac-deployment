c773fbc8cdd333ae2b8314472fd30b754c617961
sumeshsuse <sumeshsuseelan.techie@gmail.com>
2026-03-24 18:13:25 +0100

    fix duplicate terraform block in dev backend config

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -5,15 +5,6 @@ terraform {
       version = "~> 1.45"
     }
   }
-}
-
-terraform {
-  required_providers {
-    hcloud = {
-      source  = "hetznercloud/hcloud"
-      version = "~> 1.45"
-    }
-  }
 
   cloud {
     organization = "QVEST"
