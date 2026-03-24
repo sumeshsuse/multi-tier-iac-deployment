887ef2515e0302c577f1311f03bf246fbe15c450
sumeshsuse <sumeshsuseelan.techie@gmail.com>
2026-03-24 18:03:29 +0100

    configure remote terraform state for dev using terraform cloud workspace

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -7,6 +7,23 @@ terraform {
   }
 }
 
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.45"
+    }
+  }
+
+  cloud {
+    organization = "QVEST"
+
+    workspaces {
+      name = "multi-tier-dev"
+    }
+  }
+}
+
 provider "hcloud" {
   token = var.hcloud_token
 }
